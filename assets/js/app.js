960ea95aaeaa8e314af496f3c425640f4072049f
kroddy95 <kroddy95@gmail.com>
2020-10-09 13:40:15 -0500

    Added arrow to the tooltip

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,13 +28,13 @@ var chartGroup = svg.append("g")
 d3.csv("../assets/data/data.csv").then(function(censusData){
   console.log(censusData);
 
-//Parse Data
-censusData.forEach(function(data){
+  //Parse Data
+  censusData.forEach(function(data){
     data.healthcare = +data.healthcare;
     data.age = +data.age;
   });
 
-//Create scale functions
+  //Create scale functions
   var xLinearScale = d3.scaleLinear()
     .domain([d3.min(censusData, d => d.healthcare)-2, d3.max(censusData, d => d.healthcare)+2])
     .range([0, width]);
@@ -43,11 +43,11 @@ censusData.forEach(function(data){
     .domain([d3.min(censusData, d => d.age)-2, d3.max(censusData, d => d.age)+2])
     .range([height, 0]);    
 
-//Create axis functions
+  //Create axis functions
   var bottomAxis = d3.axisBottom(xLinearScale);
   var leftAxis = d3.axisLeft(yLinearScale);
 
-//Append Axis to the chart
+  //Append Axis to the chart
   chartGroup.append("g")
     .attr("transform", `translate(0, ${height})`)
     .call(bottomAxis);
@@ -55,8 +55,8 @@ censusData.forEach(function(data){
     chartGroup.append("g")  
     .call(leftAxis);
 
-// Create Cirlces
-    var circlesGroup = chartGroup.selectAll("circle")
+  // Create Cirlces
+  var circlesGroup = chartGroup.selectAll("circle")
     .data(censusData)
     .enter()
     .append("circle")
@@ -66,7 +66,7 @@ censusData.forEach(function(data){
     .attr("opacity", "1")
     .attr('class', 'stateCircle');
 
-    texts = chartGroup.append("g").selectAll("text")
+  chartGroup.append("g").selectAll("text")
     .data(censusData)
     .enter()
     .append('text')
@@ -77,40 +77,41 @@ censusData.forEach(function(data){
       return d.abbr;
     })
    
-
-//Initialize tool tip
+  //Initialize tool tip
   var toolTip = d3.tip()
     .attr("class", "d3-tip")
-    .offset([70, -60])
+    .offset([-5, 0])
     .html(function(d){ 
-      return d.state +'<br>Healthcare: ' + d.healthcare + '% <br> Median Age: ' + d.age;
+      return d.state +'<br>Without Healthcare: ' + d.healthcare + '% <br> Median Age: ' + d.age;
     });
 
-//Create tooltop in the chart
-    chartGroup.call(toolTip);
+  //Create tooltop in the chart
+  chartGroup.call(toolTip);
 
-//Create event listeners to display and hide the tooltip
-    circlesGroup.on("mouseover", function(data){
-      toolTip.show(data, this);
-    })
+  //Create event listeners to show and hide tooltip
+  circlesGroup.on("mouseover", function(data){
+    toolTip.show(data, this);
+  })
 
-    .on("mouseout", function(data){
-      toolTip.hide(data);
-    });
+  .on("mouseout", function(data){
+    toolTip.hide(data);
+  });
 
-    // Create axis labels
-    chartGroup.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left + 40)
-      .attr("x", 0 - (height /2))
-      .attr("dy", "1em")
-      .attr("class", "axisText")
-      .text("Median Age");
+  // Create axis labels
+  chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left + 40)
+    .attr("x", 0 - (height /2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("Median Age");
 
-    chartGroup.append("text")
-      .attr("transform", `translate(${width/2}, ${height + margin.top +30})`)
-      .attr("class", "axisText")
-      .text("Percent without Healthcare")
+  chartGroup.append("text")
+    .attr("transform", `translate(${width/2}, ${height + margin.top +30})`)
+    .attr("class", "axisText")
+    .text("Percent without Healthcare")
+
+  //Catch the error
   }).catch(function(error) {
     console.log(error);
   });
